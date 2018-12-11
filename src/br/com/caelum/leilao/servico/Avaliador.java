ce89603a7b8fdfec0d59664b974a45133d09c6c0
jhonataribeiro <jhonata.ribeiro.ti@gmail.com>
2018-12-11 13:23:46 -0200

    Adiciona testes com valores randomicos

--- src/br/com/caelum/leilao/servico/Avaliador.java
+++ src/br/com/caelum/leilao/servico/Avaliador.java
@@ -3,6 +3,9 @@ package br.com.caelum.leilao.servico;
 import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 /**
  * @author jribeiro
  * @date 09/12/18
@@ -16,26 +19,40 @@ public class Avaliador {
         for (Lance lance : leilao.getLances()) {
             if (lance.getValor() > maiorLance) {
                 maiorLance = lance.getValor();
-            } if (lance.getValor() < menorLance) {
+            }
+            if (lance.getValor() < menorLance) {
                 menorLance = lance.getValor();
             }
         }
     }
 
-    public double getMaiorLance(){
+    public double getMaiorLance() {
         return maiorLance;
     }
 
-    public double getMenorLance(){
+    public double getMenorLance() {
         return menorLance;
     }
 
     public double getMediaLances(Leilao leilao) {
         int numeroLances = 0;
-        for (Lance lance: leilao.getLances()){
+        for (Lance lance : leilao.getLances()) {
             totalLances = totalLances + lance.getValor();
             numeroLances += 1;
         }
-        return totalLances/numeroLances;
+        return totalLances / numeroLances;
+    }
+
+    public List<Lance> getMaioresLances(Leilao leilao, int numeroLances) {
+        List<Lance> maiores = new ArrayList<Lance>(leilao.getLances());
+        Collections.sort(maiores, new Comparator<Lance>() {
+            @Override
+            public int compare(Lance lance, Lance t1) {
+                if(lance.getValor() < t1.getValor()) return 1;
+                if(lance.getValor() > t1.getValor()) return -1;
+                return 0;
+            }
+        });
+        return maiores.subList(0,numeroLances);
     }
 }
--- test/br/com/caelum/leilao/servico/AvaliadorTest.java
+++ test/br/com/caelum/leilao/servico/AvaliadorTest.java
@@ -2,8 +2,14 @@ import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.dominio.Usuario;
 import br.com.caelum.leilao.servico.Avaliador;
-import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
 
 
 /**
@@ -13,12 +19,12 @@ import org.junit.Test;
 public class AvaliadorTest {
 
     @Test
-    public void avaliador(){
+    public void avaliador() {
         //Avaliador
         Leilao leilao = new Leilao("Carro usado");
-        leilao.propoe(new Lance(new Usuario(1,"joão"), 20.000));
-        leilao.propoe(new Lance(new Usuario(2,"Maria"), 29.000));
-        leilao.propoe(new Lance(new Usuario(3,"Pedro"), 35.000));
+        leilao.propoe(new Lance(new Usuario(1, "joão"), 20.000));
+        leilao.propoe(new Lance(new Usuario(2, "Maria"), 29.000));
+        leilao.propoe(new Lance(new Usuario(3, "Pedro"), 35.000));
 
         Avaliador avaliador = new Avaliador();
         avaliador.avalia(leilao);
@@ -26,23 +32,111 @@ public class AvaliadorTest {
         double maiorEsperado = 35;
         double menorEsperado = 20;
 
-        Assert.assertEquals(maiorEsperado,avaliador.getMaiorLance(),000000001);
-        Assert.assertEquals(menorEsperado,avaliador.getMenorLance(), 000000001);
+        assertEquals(maiorEsperado, avaliador.getMaiorLance(), 000000001);
+        assertEquals(menorEsperado, avaliador.getMenorLance(), 000000001);
     }
 
     @Test
-    public void obterMediaLances(){
+    public void obterMediaLances() {
         //Avaliador
         Leilao leilao = new Leilao("Carro usado");
-        leilao.propoe(new Lance(new Usuario(1,"joão"), 20.000));
-        leilao.propoe(new Lance(new Usuario(2,"Maria"), 29.000));
-        leilao.propoe(new Lance(new Usuario(3,"Pedro"), 35.000));
+        leilao.propoe(new Lance(new Usuario(1, "joão"), 20.000));
+        leilao.propoe(new Lance(new Usuario(2, "Maria"), 29.000));
+        leilao.propoe(new Lance(new Usuario(3, "Pedro"), 35.000));
 
         Avaliador avaliador = new Avaliador();
         avaliador.avalia(leilao);
 
-        double mediaEsperada = (20.000 + 29.000 + 35.000)/3;
+        double mediaEsperada = (20.000 + 29.000 + 35.000) / 3;
 
-        Assert.assertEquals(mediaEsperada,avaliador.getMediaLances(leilao),000000001);
+        assertEquals(mediaEsperada, avaliador.getMediaLances(leilao), 000000001);
     }
+
+    @Test
+    public void deveEntenderLeilaoComApenasUmLance() {
+        Leilao leilao = new Leilao("Carro usado 2013");
+        leilao.propoe(new Lance(new Usuario(1, "joão"), 200));
+        Avaliador avaliador = new Avaliador();
+        avaliador.avalia(leilao);
+
+        double valorEsperado = 200;
+
+        assertEquals(valorEsperado, avaliador.getMaiorLance(), 000000001);
+        assertEquals(valorEsperado, avaliador.getMenorLance(), 000000001);
+    }
+
+    @Test
+    public void deveRetornarTresMarioresLancesEmOrdemDecrescente() {
+        Leilao leilao = new Leilao("Carro usado");
+
+        Usuario joao = new Usuario(1, "João");
+        Usuario maria = new Usuario(2, "Maria");
+        Usuario pedro = new Usuario(3, "Pedro");
+
+        leilao.propoe(new Lance(joao, 20.000));
+        leilao.propoe(new Lance(maria, 29.000));
+        leilao.propoe(new Lance(pedro, 35.000));
+        leilao.propoe(new Lance(joao, 45.000));
+        leilao.propoe(new Lance(maria, 55.000));
+
+        List<Lance> lances = Arrays.asList(
+                new Lance(maria, 55.000),
+                new Lance(joao, 45.000),
+                new Lance(pedro, 35.000)
+        );
+
+
+        Avaliador avaliador = new Avaliador();
+        List<Lance> lancesRetornados = avaliador.getMaioresLances(leilao, 3);
+
+
+        assertEquals(lances.size(), lancesRetornados.size(), 0.00000001);
+        assertEquals(55.000, lancesRetornados.get(0).getValor(), 0000001);
+        assertEquals(45.000, lancesRetornados.get(1).getValor(), 0000001);
+        assertEquals(35.000, lancesRetornados.get(2).getValor(), 0000001);
+    }
+
+    @Test
+    public void avaliadorDeveFuncionarComLancesRandomicos() {
+        Random random = new Random();
+        List<Leilao> leilaos = new ArrayList<Leilao>();
+        Leilao leilao = new Leilao("Carro usado");
+
+        Usuario joao = new Usuario(1, "João");
+        Usuario maria = new Usuario(2, "Maria");
+        Usuario pedro = new Usuario(3, "Pedro");
+
+        leilao.propoe(new Lance(joao, 200));
+        leilao.propoe(new Lance(maria, 690));
+        leilao.propoe(new Lance(pedro, 150));
+        leilao.propoe(new Lance(joao, 450));
+        leilao.propoe(new Lance(maria, 550));
+
+
+
+//        for (int i=0;i < 11; i++){
+//            leilao.propoe(new Lance(this.obterUsuarioAleatoriamente(), random.nextInt(200 - 100 ) + 100));
+//        }
+
+        Avaliador avaliador = new Avaliador();
+//        List<Lance> lancesRetornados = avaliador.getMaioresLances(leilao, 3);
+        avaliador.avalia(leilao);
+        assertEquals(690, avaliador.getMaiorLance(), 000000001);
+        assertEquals(150, avaliador.getMenorLance(), 000000001);
+    }
+
+    public Usuario obterUsuarioAleatoriamente() {
+        Random random = new Random();
+
+        List<Usuario> usuarios = Arrays.asList(
+                new Usuario(2, "Maria"),
+                new Usuario(1, "João"),
+                new Usuario(3, "Pedro")
+        );
+
+        return usuarios.get(random.nextInt(2 - 0 ) + 0);
+    }
+
+
+
 }
